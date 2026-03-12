d2d85d62a556c0c6a20917fc7b6ea2d7e2d10ced
yashwanth <yashwanthkumar6362@gmail.com>
2026-03-12 00:19:30 -0400

    Logging in SpringBoot

--- week-04-prod-ready-features/src/main/java/com/codingshuttle/prod_ready_features/client/impl/EmployeeClientImpl.java
+++ week-04-prod-ready-features/src/main/java/com/codingshuttle/prod_ready_features/client/impl/EmployeeClientImpl.java
@@ -5,6 +5,8 @@ import com.codingshuttle.prod_ready_features.advice.ApiResponse;
 import com.codingshuttle.prod_ready_features.client.EmployeeClient;
 import com.codingshuttle.prod_ready_features.dto.EmployeeDTO;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.stereotype.Service;
@@ -17,49 +19,67 @@ public class EmployeeClientImpl implements EmployeeClient {
 
     private final RestClient restClient;
 
+    Logger log = LoggerFactory.getLogger(EmployeeClientImpl.class);
+
     public EmployeeClientImpl(RestClient restClient){
         this.restClient = restClient;
     }
 
-    public ApiResponse<List<EmployeeDTO>> getAllEmployee(){
-        try{
-            return restClient.get()
+    public ApiResponse<List<EmployeeDTO>> getAllEmployee() {
+        log.info("Entered into getAllEmployee, trying retrieve response");
+        try {
+            ApiResponse<List<EmployeeDTO>> response = restClient.get()
                     .uri("employees")
                     .retrieve()
+                    .onStatus(HttpStatusCode::is4xxClientError,(req,res)->{
+                        log.error("Couldn't get all employees data from client: {}" , new String(res.getBody().readAllBytes()));
+                        throw new ResourceNotFoundException("couldn't find employees");
+                    })
                     .body(new ParameterizedTypeReference<ApiResponse<List<EmployeeDTO>>>() {
                     });
-        }catch (Exception e){
+            log.trace("response successfully retrieved at getAllEmployees: {}",response.toString());
+            return response;
+        } catch (Exception e) {
+            log.error("Exception occurred at getAllEmployees: ", e);
             throw new RuntimeException(e);
         }
     }
 
     public ApiResponse<EmployeeDTO> getEmpByID(Long id){
+        log.info("Entered into getEmpByID, trying retrieve response for ID: {}",id);
         try{
             return restClient.get()
                     .uri("employees/{empId}",id)
                     .retrieve()
                     .onStatus(HttpStatusCode::is4xxClientError,(req,res)->{
-                        System.out.println("Error occurred:" + res.getBody().readAllBytes());
+                        log.error("Couldn't get emp data from client: {}" , new String(res.getBody().readAllBytes()));
                         throw new ResourceNotFoundException("couldn't find employee");
                     })
                     .body(new ParameterizedTypeReference<ApiResponse<EmployeeDTO>>(){
             });
         }catch (Exception e){
+            log.error("Exception occurred at getEmpById: ", e);
             throw new RuntimeException(e);
         }
     }
 
 
     public ApiResponse<EmployeeDTO> createEmployee(EmployeeDTO input){
+        log.info("Entered into createEmployee: {}",input);
         try{
             return restClient.post()
                     .uri("employees")
                     .body(input)
                     .retrieve()
+                    .onStatus(HttpStatusCode::is4xxClientError,(req,res)->{
+                        log.error("Couldn't create emp data into client: {}" , new String(res.getBody().readAllBytes()));
+                        throw new ResourceNotFoundException("couldn't create employee");
+                    })
                     .body(new ParameterizedTypeReference<ApiResponse<EmployeeDTO>>(){
 
                     });
         }catch (Exception e){
+            log.error("Exception occurred at createEmployee: ", e);
             throw new RuntimeException(e);
         }
     }
--- week-04-prod-ready-features/src/main/java/com/codingshuttle/prod_ready_features/dto/EmployeeDTO.java
+++ week-04-prod-ready-features/src/main/java/com/codingshuttle/prod_ready_features/dto/EmployeeDTO.java
@@ -1,9 +1,6 @@
 package com.codingshuttle.prod_ready_features.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import java.time.LocalDate;
 
@@ -12,6 +9,7 @@ import java.time.LocalDate;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@ToString
 public class EmployeeDTO {
 
     private Long id;
